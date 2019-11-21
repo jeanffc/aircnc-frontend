63fb6a6889fb74bf9b99b434292062ce2ebb3cb2
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-11-20 21:46:23 -0300

    feat: added app screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+      <div className="content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ofereça <strong>spots</strong> para programadores e encontre{" "}
+          <strong>talentos</strong> para sua empresa
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form>
+          <label htmlFor="email">E-mail *</label>
+          <input id="email" type="email" placeholder="Seu melhor e-mail" />
+          <button className="btn" type="submit">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
