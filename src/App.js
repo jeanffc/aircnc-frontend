6b0abb07337742957532952bc83e21a457262241
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-11-21 13:05:25 -0300

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
-import React, { useState } from "react";
-import api from "./services/api";
+import React from "react";
 
 import "./App.css";
 
 import logo from "./assets/logo.svg";
 
+import Routes from "./routes";
+
 function App() {
-  const [email, setEmail] = useState("");
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const response = await api.post("/sessions", { email });
-
-    const { _id } = response.data;
-
-    localStorage.setItem("user", _id);
-
-    console.log(_id);
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre{" "}
-          <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-mail *</label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-          <button className="btn" type="submit">
-            Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
