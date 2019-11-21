3b3818fa511dba961faca4a07f1429b630fccc64
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-11-20 22:05:08 -0300

    feat: added api integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "./services/api";
+
 import "./App.css";
 
 import logo from "./assets/logo.svg";
 
 function App() {
+  const [email, setEmail] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post("/sessions", { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem("user", _id);
+
+    console.log(_id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
@@ -13,9 +29,15 @@ function App() {
           <strong>talentos</strong> para sua empresa
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail *</label>
-          <input id="email" type="email" placeholder="Seu melhor e-mail" />
+          <input
+            id="email"
+            type="email"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
           <button className="btn" type="submit">
             Entrar
           </button>
